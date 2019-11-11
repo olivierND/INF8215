712fedd1a730fb20d561eda26c87b05b67ee3373
Olivier Naud-Dulude <olivier.naud-dulude@polymtl.ca>
2019-11-10 23:30:58 -0500

    expectimax

--- TP2/MiniMaxSearch.py
+++ TP2/MiniMaxSearch.py
@@ -1,5 +1,13 @@
 import random
-import numpy as np
+import numpy
+from enum import Enum
+
+
+class Expectimax(Enum):
+    Random = 0,
+    Optimistic = 1,
+    Pessimistic = 2
+
 
 class MiniMaxSearch:
     def __init__(self, rushHour, initial_state, search_depth):
@@ -49,15 +57,20 @@ class MiniMaxSearch:
             state.score = state.score_state(self.rushhour)
 
             if is_max and state.score >= score:
-                score = state.score
-                s.score = score
-                possible_states.append(s)
-            elif not is_max and state.score <= score:
+                if state.score > score:
+                    possible_states.clear()
                 score = state.score
                 s.score = score
                 possible_states.append(s)
 
-        return random.choice([s for s in possible_states if s.score == score])
+            elif not is_max and state.score <= score:
+                if state.score < score:
+                    possible_states.clear()
+                score = state.score
+                s.score = score
+                possible_states.append(s)
+
+        return random.choice(possible_states)
 
     def minimax_pruning(self, current_depth, current_state, is_max, alpha, beta):
         if current_depth == self.search_depth:
@@ -80,25 +93,85 @@ class MiniMaxSearch:
             state.score = state.score_state(self.rushhour)
 
             if is_max:
+                alpha = max(alpha, state.score)
+
                 if state.score >= score:
+                    if state.score > score:
+                        possible_states.clear()
                     score = state.score
                     s.score = score
                     possible_states.append(s)
+
                 if score >= beta:
                     s.score = score
                     return s
-                alpha = max(alpha, state.score)
             else:
+                beta = max(beta, state.score)
+
                 if state.score <= score:
+                    if state.score < score:
+                        possible_states.clear()
                     score = state.score
                     s.score = score
                     possible_states.append(s)
+
                 if alpha >= score:
                     s.score = score
                     return s
-                beta = max(beta, state.score)
 
-        return random.choice([s for s in possible_states if s.score == score])
+        return random.choice(possible_states)
+
+    def expectimax(self, current_depth, current_state, is_max, mode):
+        if current_depth == self.search_depth:
+            return current_state
+
+        if is_max:
+            possible_moves = self.rushhour.possible_rock_moves(current_state)
+            score = float('-inf')
+        else:
+            possible_moves = self.rushhour.possible_moves(current_state)
+            score = float('inf')
+
+        possible_states = []
+        possible_states_probability = []
+        total_score = 0
+
+        for s in possible_moves:
+            if s.success() and current_depth != self.search_depth:
+                return s
+
+            state = self.expectimax(current_depth + 1, s, not is_max, mode)
+            state.score = state.score_state(self.rushhour)
+
+            if is_max:
+                s.score = state.score
+                possible_states.append(s)
+                total_score += state.score
+
+            elif not is_max and state.score <= score:
+                if state.score < score:
+                    possible_states.clear()
+                score = state.score
+                s.score = score
+                possible_states.append(s)
+
+        if is_max:
+            if mode == Expectimax.Random or total_score == 0:
+                return random.choice(possible_states)
+
+            elif mode == Expectimax.Pessimistic:
+                for state in possible_states:
+                    possible_states_probability.append(state.score / total_score)
+                return numpy.random.choice(possible_states, p=possible_states_probability)
+
+            elif mode == Expectimax.Optimistic:
+                possible_states.sort(key=lambda x: x.score)
+                for state in possible_states:
+                    possible_states_probability.append(state.score / total_score)
+                return numpy.random.choice(possible_states, p=possible_states_probability.reverse())
+
+        else:
+            return random.choice(possible_states)
 
     def solve(self, state, is_singleplayer, is_pruning):
         if state.success():
