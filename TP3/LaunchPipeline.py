3cde4efda41ebb6bdd06079f9a50fb53416ffd31
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2019-12-02 13:18:55 -0500

    suite preprocessing

--- TP3/LaunchPipeline.py
+++ TP3/LaunchPipeline.py
@@ -1,24 +1,58 @@
 import pandas as pd
-from sklearn.pipeline import Pipeline, FeatureUnion
+import numpy as np
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from TransformationWrapper import TransformationWrapper
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+
+from TP3.TransformationWrapper import TransformationWrapper, LabelEncoderP
 
 PATH = "data/"
 X_train = pd.read_csv(PATH + "train.csv")
 X_test = pd.read_csv(PATH + "test.csv")
 
 
-pipeline_feature1 = Pipeline([
-    ("name", TransformationWrapper()),
+def parse_unknown(text):
+    if "?" in text:
+        return "Unknown"
+    return text
+
+
+def parse_income(text):
+    if "." in text:
+        return text[:-1]
+    return text
+
+
+pipeline_workclass = Pipeline([
+    ('unknown', TransformationWrapper(transformation=parse_unknown))
 ])
 
+pipeline_income = Pipeline([
+    ('income', TransformationWrapper(transformation=parse_income))
+])
+
+# pour normaliser
+pipeline_numerical = Pipeline([
+    ("fillna", SimpleImputer(strategy='mean')),
+    ("scaler", StandardScaler())
+])
+
+# pour binariser
+pipeline_binary = Pipeline([
+    ("encode", OneHotEncoder(categories='auto', sparse=False))
+])
 
 full_pipeline = ColumnTransformer([
-        ("workclass", pipeline_feature1, ["Workclass"])
-        # ("feature2", pipeline_feature2, ["Feature2"]),
-    ])
+    ("Age", pipeline_numerical, ["Age"]),
+    ("Workclass", pipeline_workclass, ["Workclass"]),
+    ("Sex", pipeline_binary, ["Sex"]),
+    ("Capital-gain", pipeline_numerical, ["Capital-gain"]),
+    ("Capital-loss", pipeline_numerical, ["Capital-loss"]),
+    ("Hours per week", pipeline_numerical, ["Hours per week"]),
+])
 
-column_names = [] # TODO ajouter les noms des colonnes selon le nouvel ordre du pipeline
+column_names = ["Age", "Workclass", "Female", "Male", "Capital-gain", "Capital-loss", "Hours per week"] # TODO ajouter les noms des colonnes selon le nouvel ordre du pipeline
 X_train_preprocess = pd.DataFrame(full_pipeline.fit_transform(X_train), columns=column_names)
 X_test_preprocess = pd.DataFrame(full_pipeline.fit_transform(X_test), columns=column_names)
+print(X_train_preprocess.head())
